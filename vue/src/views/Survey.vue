d10acad4696b712120dfd80808343746b851973d
sohanur.rahman <sohanur.rahaman@btracsl.com>
2022-03-23 23:13:48 +0600

    edit delete survey start

--- vue/src/views/Survey.vue
+++ vue/src/views/Survey.vue
@@ -30,8 +30,21 @@
                              class="flex py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600
                              hover: bg-indigo-700 focus: ring2 focus:ring-offset-2 focus: ring-indigo-500"
                 >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                    </svg>
                 Edit
                 </router-link>
+                <button v-if="survey.id"
+                type="button" @click="deleteSurvey(survey)"
+                class="h-8 w-8 flex items-center justify-center
+rounded-full border border-transparent
+text-sm text-red-500 focus:ring-2 focus:ring-offset-2
+focus:ring-red-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
+                </button>
             </div>
         </div>
     </div>
@@ -45,5 +58,9 @@ import {computed} from "vue";
 import PageComponent from "../components/PageComponent.vue";
 
 const surveys = computed(() => store.state.surveys);
-
+function deleteSurvey(survey) {
+    if (confirm(`Are you want to delete this survey? Operation can't be undone!`)){
+        //delete survey
+    }
+}
 </script>
--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -1,13 +1,43 @@
 <template>
+<PageComponent title="View or create a survey">
+    <template v-slot:header>
+        <div class="flex items-center justify-between">
+            <h1 class="text-3x1 font-bold text-gray-900">
+                {{ model.id ? model.title : "Create a Survey" }}
+            </h1>
+        </div>
+    </template>
+    <form @submit.prevent="saveSurvey">
+        <div class="shadow sm:rounded-md sm:overflow-hidden">
+            <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
 
+            </div>
+        </div>
+    </form>
+</PageComponent>
 </template>
 
-<script>
-export default {
-    name: "SurveyView"
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+import {ref} from "vue";
+import store from "../store";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+
+//create empty survey
+let model = ref({
+    title: "",
+    status: false,
+    description: null,
+    image: null,
+    expire_date: null,
+    question: []
+});
+
+if (route.params.id) {
+    model.value = store.state.surveys.find(
+        (s) => s.id === parseInt(route.params.id)
+    );
 }
 </script>
-
-<style scoped>
-
-</style>

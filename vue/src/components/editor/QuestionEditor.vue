fb12630d7e0206ba7b7bbe5e8c07275282389c4c
sohanur.rahman <sohanur.rahaman@btracsl.com>
2022-03-27 17:59:17 +0600

    survey add update form

---
+++ vue/src/components/editor/QuestionEditor.vue
@@ -0,0 +1,71 @@
+<template>
+<!--   Question index-->
+    <div class="flex items-center justify-between">
+        <h3 class="text-lg font-bold">
+            {{ index + 1 }}. {{ model.question }}
+        </h3>
+
+        <div class="flex items-center">
+            <!-- Add New Question-->
+                <button type="button" @click="addQuestion()" class="flex items-center text-xs py-1 px-3 mr-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block -mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                   </svg>
+                    Add
+                </button>
+            <!-- Add New Question-->
+
+            <!--Delete Question-->
+            <button type="button" @click="deleteQuestion()" class="flex items-center text-xs py-1 px-3 mr-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                Delete
+            </button>
+            <!--Delete Question-->
+        </div>
+    </div>
+<!--    Question index-->
+    <div class="grid gap-3 grid-cols-12">
+        <!--Question-->
+        <div class="mt-3 col-span-9">
+            <label
+            :for="'question_text_' + model.data"
+            class="block text-sm font-medium text-gray-700">
+                Question Text
+            </label>
+            <input
+            type="text"
+            :name="'question_text_' + model.data"
+            v-model="model.question"
+            @change="dataChange"
+            :id="'question_text_' + model.data"
+            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+        </div>
+
+        <!--Question-->
+
+        <!--Question Type-->
+
+        <!--Question Type-->
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps({
+    question: Object,
+    index: Number
+});
+
+const emit = defineEmits(["change", "addQuestion", "deleteQuestion"])
+
+//Re-create the whole question data to avoid unintentional reference change
+const model = ref(JSON.parse(JSON.stringify(props.question)));
+</script>
+
+<style>
+
+</style>
--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -10,6 +10,112 @@
     <form @submit.prevent="saveSurvey">
         <div class="shadow sm:rounded-md sm:overflow-hidden">
             <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                <!--Image-->
+                <div>
+                    <label class="text-left block  text-sm font-medium text-gray-700">
+                        Image
+                    </label>
+                    <div class="mt-1 flex items-center">
+                        <img
+                            v-if="model.image"
+                            :src="model.image"
+                            :alt="model.title"
+                            class="w-64 h-48 object-cover"
+                        />
+                        <span v-else class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                            <svg class="h-[80%] w-[80%] text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                        </span>
+                        <button type="button" class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm
+                                text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <input type="file" class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"/>
+                            Change
+                        </button>
+                    </div>
+                </div>
+                <!--end image-->
+
+                <!--Title-->
+                    <div>
+                        <label for="title" class="block text-left text-sm font-medium text-gray-700">Title</label>
+                        <input type="text" name="title" id="title" v-model="model.title" autocomplete="survey_title"
+                               class=" mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                    </div>
+                <!--end Title-->
+
+                <!--Description-->
+                <div>
+                    <label for="description" class="block text-left text-sm font-medium text-gray-700">Description</label>
+                    <div class="mt-1">
+                        <textarea id="description" name="description"
+                        rows="3" v-model="model.description"
+                        autocomplete="survey_description"
+                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        placeholder="Enter your survey description">
+                        </textarea>
+
+                    </div>
+                </div>
+                <!--end Description-->
+
+                <!-- Expiry Date-->
+                <div>
+                    <label for="expire_date" class="block text-left text-sm font-medium text-gray-700">Expire Date</label>
+                    <input type="date" name="expire_date" id="expire_date" v-model="model.expire_date" autocomplete="survey_expire_date"
+                           class=" mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+                <!--Expiry Date-->
+
+                <!-- Status-->
+                <div class="flex items-start">
+                    <div class="flex items-center h-5">
+                        <input
+                        id="status"
+                        name="status"
+                        type="checkbox"
+                        v-model="model.status"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                        >
+                    </div>
+                    <div class="ml-3 text-sm">
+                        <label for="status" class="font-medium text-gray-700">Active</label>
+                    </div>
+                </div>
+                <!--Status-->
+
+                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                    <h3 class="text-2x1 font-semibold flex items-center justify-between">
+                        Questions
+
+                        <button type="button" @click="addQuestion()" class="flex items-center text-sm py-1 px-4 rounded-md text-white bg-gray-600 hover:bg-gray-700" >
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block -mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                            </svg> Add Question
+                        </button>
+                    </h3>
+                    <div v-if="!model.questions.length"
+                    class="text-center text-gray-600">
+                        You don't have any questions created
+                    </div>
+                    <div v-for="(question,index) in model.questions" :key="question.id">
+                        <QuestionEditor
+                            :question="question"
+                            :index="index"
+                            @change="questionChange"
+                            @addQuestion="addQuestion"
+                            @deleteQuestion="deleteQuestion"
+                        />
+                    </div>
+                </div>
+
+                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md
+                        text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Save
+                    </button>
+
+                </div>
 
             </div>
         </div>
@@ -19,6 +125,8 @@
 
 <script setup>
 import PageComponent from "../components/PageComponent.vue";
+import QuestionEditor from "../components/editor/QuestionEditor.vue";
+
 import {ref} from "vue";
 import store from "../store";
 import {useRoute} from "vue-router";
@@ -27,12 +135,12 @@ const route = useRoute();
 
 //create empty survey
 let model = ref({
-    title: "",
+    title: null,
     status: false,
     description: null,
     image: null,
     expire_date: null,
-    question: []
+    questions: []
 });
 
 if (route.params.id) {

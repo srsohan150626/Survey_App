fa1c61f3d49b4e9444fad22eedf392b7bd1a7c4a
sohanur.rahman <sohanur.rahaman@btracsl.com>
2022-03-28 14:47:42 +0600

    survey view

--- vue/src/components/editor/QuestionEditor.vue
+++ vue/src/components/editor/QuestionEditor.vue
@@ -29,9 +29,7 @@
     <div class="grid gap-3 grid-cols-12">
         <!--Question-->
         <div class="mt-3 col-span-9">
-            <label
-            :for="'question_text_' + model.data"
-            class="block text-sm font-medium text-gray-700">
+            <label :for="'question_text_' + model.data" class="text-left block text-sm font-medium text-gray-700">
                 Question Text
             </label>
             <input
@@ -47,13 +45,65 @@
         <!--Question-->
 
         <!--Question Type-->
-
+        <div class="mt-3 col-span-3">
+            <label for="question_type" class="block text-sm font-medium text-gray-700">Select Question Type</label>
+            <select id="question_type" name="question_type" v-model="model.type" @change="typeChange" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <option v-for="type in questionTypes" :key="type" :value="type">
+                    {{ upperCaseFirst(type) }}
+                </option>
+            </select>
+        </div>
         <!--Question Type-->
     </div>
+      <!--Question Description-->
+            <div class="mt-3 col-span-9">
+                <label :for="'question_description_' + model.id" class="text-left block text-sm font-medium text-gray-700">
+                    Description
+                </label>
+                <textarea :name="'question_description_' + model.id" v-model="model.description" @change="dataChange"
+                :id="'question_description_' + model.id"
+                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                >
+                </textarea>
+            </div>
+      <!--Question Description-->
+      <!--Data-->
+    <div v-if="shouldHaveOptions()" class="mt-2">
+        <h4 class="text-sm font-semibold mb-1 flex justify-between items-center">
+            Options
+        <!--Add New Option-->
+            <button type="button" @click="addOption()" class="flex items-center text-xs py-1 px-2 rounded-md text-white bg-gray-600 hover:bg-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block -mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                </svg> Add Option
+            </button>
+        <!--Add New Option-->
+        </h4>
+        <div v-if="!model.data.options.length" class="text-xs text-gray-600 text-center py-3">
+            You don't have any options defined
+        </div>
+        <!--Option List-->
+        <div v-for="(option, index) in model.data.options" :key="option.uuid" class="flex items-center mb-1">
+            <span class="w-6 text-sm"> {{ index+1 }}.</span>
+            <input type="text" v-model="option.text" @change="dataChange" class="w-full rounded-sm py-1 px-2 text-xs border border-gray-300 focus:border-indigo-500">
+        </div>
+        <!--Option List-->
+
+        <!--Delete Option-->
+         <button type="button" @click="removeOption(option)" class="h-6 w-6 rounded-full flex items-center justify-center border border-transparent transition-colors hover:border-red-100">
+             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+             </svg>
+         </button>
+        <!--Delete Option-->
+    </div>
+      <!--Data-->
+    <hr class="my-4" />
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import store from "../../store";
 
 const props = defineProps({
     question: Object,
@@ -64,6 +114,53 @@ const emit = defineEmits(["change", "addQuestion", "deleteQuestion"])
 
 //Re-create the whole question data to avoid unintentional reference change
 const model = ref(JSON.parse(JSON.stringify(props.question)));
+
+//Get question types from vuex
+const questionTypes = computed(() => store.state.questionTypes);
+
+function upperCaseFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function shouldHaveOptions() {
+    return ["select", "radio", "checkbox"].includes(model.value.type);
+}
+
+function getOptions() {
+    return model.value.data.options;
+}
+
+function setOptions() {
+     model.value.data.options = options;
+}
+
+function addOption() {
+    setOptions([
+        ...getOptions(),
+        { uuid: uuidv4(), text: ""},
+    ]);
+    dataChange();
+}
+
+function removeOption(op) {
+    setOptions(getOptions().filter((opt) => opt !== op));
+    dataChange();
+}
+
+function typeChange() {
+    if(shouldHaveOptions()) {
+        setOptions(getOptions() || []);
+    }
+    dataChange();
+}
+
+function dataChange() {
+    const data = model.value;
+    if(!shouldHaveOptions()) {
+        delete data.data.options;
+    }
+}
+
 </script>
 
 <style>
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -221,6 +221,7 @@ const store = createStore({
       token: sessionStorage.getItem('TOKEN'),
     },
       surveys: [...tmpSurveys],
+      questionTypes: ["text", "select", "radio", "checkbox", "textarea"],
   },
   getters: {},
   actions: {
